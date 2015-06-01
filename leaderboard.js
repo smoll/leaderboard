ef860c53695a227262434a6057a64416a1c86a19
smoll <smollah@theorchard.com>
2015-06-01 16:32:27 -0400

    Highlight player on click

--- leaderboard.js
+++ leaderboard.js
@@ -5,8 +5,18 @@ if(Meteor.isClient){
         'player': function(){
             return PlayersList.find()
         },
-        'otherHelperFunction': function(){
-            return 'Some other function'
+        'selectedClass': function(){
+            var playerId = this._id;
+            var selectedPlayer = Session.get('selectedPlayer');
+            if(playerId == selectedPlayer){
+                return 'selected'
+            }
+        }
+    });
+    Template.leaderboard.events({
+        'click .player': function(){
+            var playerId = this._id;
+            Session.set('selectedPlayer', playerId);
         }
     });
 }
