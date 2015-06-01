d5f40861f0df3d13755abb15387685f6c16e849d
smoll <smollah@theorchard.com>
2015-06-01 16:44:26 -0400

    Add increment & decrement buttons

--- leaderboard.js
+++ leaderboard.js
@@ -17,6 +17,14 @@ if(Meteor.isClient){
         'click .player': function(){
             var playerId = this._id;
             Session.set('selectedPlayer', playerId);
+        },
+        'click .increment': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+        },
+        'click .decrement': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.update(selectedPlayer, {$inc: {score: -5} });
         }
     });
 }
