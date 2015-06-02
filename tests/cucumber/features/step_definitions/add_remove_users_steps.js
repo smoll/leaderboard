4964f6ca95804b1f5ce6ba9e27b05cf9977ae18a
smoll <smollah@theorchard.com>
2015-06-02 12:50:52 -0400

    add assertion to add player test

--- tests/cucumber/features/step_definitions/add_remove_users_steps.js
+++ tests/cucumber/features/step_definitions/add_remove_users_steps.js
@@ -4,7 +4,10 @@
 
   module.exports = function () {
 
-    var timestampedPlayerName = 'jimbo' + Math.floor(Date.now() / 1000)
+    var chai     = require('chai'),
+        expect   = chai.expect,
+        someName = 'jimbo' + Math.floor(Date.now() / 1000);
+    chai.use(require('chai-string'));
 
     this.Given(/^I am an admin user$/, function (callback) {
       // not sure how to use DDP yet
@@ -13,14 +16,16 @@
 
     this.When(/^I add a new player$/, function (callback) {
       this.client
-        .setValue('[name=playerName]', timestampedPlayerName)
+        .setValue('[name=playerName]', someName)
         .submitForm('form:first-of-type') // replace with HTML id
         .call(callback);
     });
 
     this.Then(/^the new player should be visible$/, function (callback) {
       // not sure how to check for # of players to increment
-      callback.pending();
+      this.client.getText('.player:last-child', function(err, text){
+        expect(text).to.startWith(someName)
+      }).call(callback);
     });
 
   };
