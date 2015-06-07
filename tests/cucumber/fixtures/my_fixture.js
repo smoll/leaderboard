95d46b36ed51c52d93d276b11bbb76baf1d02eec
Shujon Mollah <smollah@theorchard.com>
2015-06-07 07:05:36 -0400

    remove all users and create test users in cucumber #reset

--- tests/cucumber/fixtures/my_fixture.js
+++ tests/cucumber/fixtures/my_fixture.js
@@ -5,12 +5,11 @@
   Meteor.methods({
     'reset' : function() {
         PlayersList.remove({}); // remove players
-        if(typeof(Meteor.users.findOne({ "emails.address" : 'admin@admin.com' })) === "undefined") {
-            Accounts.createUser({ // create test user
-                email: 'admin@admin.com',
-                password: 'admin123'
-            });
-        }
+        Meteor.users.remove({}); // remove users
+        Accounts.createUser({ // create test user
+            email: 'admin@admin.com',
+            password: 'admin123'
+        });
     }
   });
 
